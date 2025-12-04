05da90b1cb7801cdb54ec12d0b78038531487659
quark.nt <quark.nt@gmail.com>
2025-12-04 23:27:19 +0300

    Added class worker

---
+++ Worker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_synergy_worker
+{
+    internal class Worker
+    {
+        private string surnameInitials;
+        private string position;
+    }
+}
