5974b7a1185b2b784fa461641bb4c79425aa4fda
quark.nt <quark.nt@gmail.com>
2025-12-09 21:26:16 +0300

    Added SetSurnameInitials

--- Worker.cs
+++ Worker.cs
@@ -2,11 +2,16 @@
 {
     internal class Worker
     {
+        //Фамилия и инициалы работника
         private string surnameInitials;
+        //Название занимаемой должности
         private string position;
+        //Зарплата
         private decimal salary;
+        //Год поступления на работу
         private DateTime employmentDate;
 
+        //Конструктор по умолчанию
         public Worker()
         {
             surnameInitials = "Unknown";
@@ -14,25 +19,25 @@
             salary = 0;
             employmentDate = new DateTime(1900, 1, 1);
         }
-
+        //Конструктор с четыремя параметрами
         public Worker(string surnameInitials, string position, decimal salary, DateTime employmentDate)
         {
             if (string.IsNullOrWhiteSpace(surnameInitials))
             {
-                throw new ArgumentException("Full name cannot be empty");
+                GetExceptionMessage("surnameInitials");
             }
             if (string.IsNullOrWhiteSpace(position))
             {
-                throw new ArgumentException("Position cannot be empty");
+                GetExceptionMessage("position");
             }
             if (salary < 0)
             {
-                throw new ArgumentException("Salary cannot by less than zero");
+                GetExceptionMessage("salary");
             }
             DateTime checkDate = new DateTime(1900, 1, 1);
             if (employmentDate < checkDate)
             {
-                throw new ArgumentException("Employment date cannot be earlier than  1 January 1995");
+                GetExceptionMessage("employmentDate");
             }
 
             this.surnameInitials = surnameInitials;
@@ -40,51 +45,80 @@
             this.salary = salary;
             this.employmentDate = employmentDate;
         }
+        //Конструктор с одним параметром
         public Worker(string surnameInitials)
         {
             if (string.IsNullOrWhiteSpace(surnameInitials))
             {
-                throw new ArgumentException("Full name cannot be empty");
+                GetExceptionMessage("surnameInitials");
             }
             this.surnameInitials = surnameInitials;
             position = "Unknown";
             salary = 0;
             employmentDate = new DateTime(1900, 1, 1);
         }
+        //Конструктор с двумя параметрами
         public Worker(string surnameInitials, string position)
         {
             if (string.IsNullOrWhiteSpace(surnameInitials))
             {
-                throw new ArgumentException("Full name cannot be empty");
+                GetExceptionMessage("surnameInitials");
             }
             if (string.IsNullOrWhiteSpace(position))
             {
-                throw new ArgumentException("Position cannot be empty");
+                GetExceptionMessage("position");
             }
             this.surnameInitials = surnameInitials;
             this.position = position;
             salary = 0;
             employmentDate = new DateTime(1900, 1, 1);
         }
+        //Конструктор с тремя параметрами
         public Worker(string surnameInitials, string position, decimal salary)
         {
             if (string.IsNullOrWhiteSpace(surnameInitials))
             {
-                throw new ArgumentException("Full name cannot be empty");
+                GetExceptionMessage("surnameInitials");
             }
             if (string.IsNullOrWhiteSpace(position))
             {
-                throw new ArgumentException("Position cannot be empty");
+                GetExceptionMessage("position");
             }
             if (salary < 0)
             {
-                throw new ArgumentException("Salary cannot by less than zero");
+                GetExceptionMessage("salary");
             }
-
             this.surnameInitials = surnameInitials;
             this.position = position;
             this.salary = salary;
             employmentDate = new DateTime(1900, 1, 1);
+        }
+        //Метод изменения фамилии и инициалов работника
+        public void SetSurnameInitial(string value)
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                this.surnameInitials = value;
+            }
+            else
+            {
+                GetExceptionMessage("surnameInitials");
+            }
+        }
+        //Вызывает ошибку в случае некорректного ввода
+        private void GetExceptionMessage(string field)
+        {
+            switch (field)
+            {
+                case "surnameInitials":
+                    throw new ArgumentException("Surname and initials cannot be empty");
+                case "position":
+                    throw new ArgumentException("Position cannot be empty");
+                case "salary":
+                    throw new ArgumentException("Salary cannot by less than zero");
+                case "employmentDate":
+                    throw new ArgumentException("Employment date cannot be earlier than  1 January 1995");
+            }
         }
     }
 }
