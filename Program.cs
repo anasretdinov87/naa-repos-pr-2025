39e6fcc38b0a8a643c65ee4ca652b959e2381fe9
quark.nt <quark.nt@gmail.com>
2025-12-10 20:42:09 +0300

    Added switch command

--- Program.cs
+++ Program.cs
@@ -2,6 +2,26 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Флаг для цикла продолжения ввода команд
+        Boolean isWorkNotComplete = true;
+        while (isWorkNotComplete) {
+            Console.WriteLine("Введите w - для ввода нового работника, d - для поиска работников с определенным стажем, exit - выход из програмы");
+            Console.Write("Ввод:");
+            string command = Console.ReadLine()??"no_command";
+            switch (command)
+            {
+                case "w":
+                    break;
+                case "d":
+                    break;
+                case "exit":
+                    isWorkNotComplete = false;
+                    break;
+                default:
+                    Console.WriteLine("Несуществующая команда. Повторите ввод.");
+                    break;
+            }
+        }
+       
     }
 }
